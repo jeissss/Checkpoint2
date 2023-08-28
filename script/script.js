c7b8b9599800c60442f0eaee6920ab623b2c7efe
jeissss <jeisongomez03@gmail.com>
2023-08-28 09:03:53 -0500

    Agregar los Dom

--- script/script.js
+++ script/script.js
@@ -26,16 +26,105 @@ function displayFooter() {
 }
 
 // Codigo DOM #1
+document.querySelector('.new-todo').addEventListener('keyup', (event) => {
+  if (
+    event.keyCode === 13 &&
+    document.querySelector('.new-todo').value.length > 0
+  ) {
+    const item = document.querySelector('.new-todo')
+    //Llamar la función que crea la tarea.**
+  }
+})
 
 // Codigo DOM #2
+// este fragmento permite conservar el estado del checkbox (true o false) en el localStorage
+
+function activateCheckboxListeners() {
+  const checkboxes = document.querySelectorAll('.toggle')
+  checkboxes.forEach((ch, i) => {
+    ch.addEventListener('click', () => {
+      itemsArray[i].checked = ch.checked
+      localStorage.setItem('items', JSON.stringify(itemsArray))
+    })
+  })
+}
 
 // Codigo DOM #3
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll('.deleteBtn')
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener('click', () => {
+      //Llamar la función que elimina la tarea
+    })
+  })
+}
 
 // Codigo DOM #4
 
+// Permite que la acción editar de las 2 listas desplegables "prioridad" y "categoría" impacte el DOM del HTML cuando cambies de opción, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
+
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll('.editBtn')
+  const updateController = document.querySelectorAll('.update-controller')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  const prioritySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[0]
+  const categorySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[1]
+
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener('click', () => {
+      updateController[i].style.display = 'block'
+      inputs[i].disabled = false
+
+      prioritySelects.value = itemsArray[i].priority
+      categorySelects.value = itemsArray[i].category
+    })
+  })
+
+  prioritySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex
+    itemsArray[i].priority = event.target.options[selectedIndex].text
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+  })
+
+  categorySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex
+    itemsArray[i].category = event.target.options[selectedIndex].text
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+  })
+}
+
 // Codigo DOM #5
+// Permite que la acción guardar el nuevo nombre de la tarea cuando decides editar y que impacte el DOM del HTML, acá debes agegar algoritmo de actualizar tarea
+
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll('.saveBtn')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener('click', () => {
+      // Llamar la función que guarda la actualización la tarea
+    })
+  })
+}
 
 // Codigo DOM #6
+// Esta es la lógica para el botón "cancelar" cuando presionas editar una tarea, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
+
+function activateCancelListeners() {
+  const cancelBtn = document.querySelectorAll('.cancelBtn')
+  const updateController = document.querySelectorAll('.update-controller')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener('click', () => {
+      updateController[i].style.display = 'none'
+      inputs[i].disabled = true
+      inputs[i].style.border = 'none'
+    })
+  })
+}
 
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
 
