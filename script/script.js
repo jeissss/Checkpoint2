8bccce6db0969c0620ca5ade03f08e1100168f7c
jeissss <jeisongomez03@gmail.com>
2023-09-04 08:45:44 -0500

    commit 2 Jeison

--- script/script.js
+++ script/script.js
@@ -1,6 +1,48 @@
 /*TIPS: *No olvides utilizar el almacenamiento local (localStorage)
  para que las tareas queden guardadas en caso
  de que la aplicación se cierre.*/
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")): []
+
+ function crearTarea(nombreTarea){
+  const objetoTarea = {
+    thing:nombreTarea,
+    checked: false,
+    priority: "Alta",
+    category: "Casa",
+    
+  }
+
+     itemsArray.push(objetoTarea)
+     localStorage.setItem("items", JSON.stringify(itemsArray))
+     location.reload()
+     
+  }
+ 
+
+ function countPend(){
+  
+  const contadorPendientes = itemsArray.filter((tarea) => tarea.checked === false);
+  //localStorage.setItem("items", JSON.stringify(itemsArray))
+  //location.reload()
+   return contadorPendientes.length;
+  
+ }
+
+
+function borrarTarea(posicion){
+itemsArray.splice(posicion,1);
+localStorage.setItem("items", JSON.stringify(itemsArray));
+location.reload();
+}
+
+function actualizarTarea(tarea, posicion){
+  itemsArray[posicion].thing = tarea;
+  localStorage.setItem("items", JSON.stringify(itemsArray))
+  location.reload()
+} 
+
+
+
 function displayFooter() {
   let page = `      
      
@@ -33,8 +75,10 @@ document.querySelector('.new-todo').addEventListener('keyup', (event) => {
   ) {
     const item = document.querySelector('.new-todo')
     //Llamar la función que crea la tarea.**
+    crearTarea(item.value)
   }
-})
+});
+
 
 // Codigo DOM #2
 // este fragmento permite conservar el estado del checkbox (true o false) en el localStorage
@@ -45,6 +89,7 @@ function activateCheckboxListeners() {
     ch.addEventListener('click', () => {
       itemsArray[i].checked = ch.checked
       localStorage.setItem('items', JSON.stringify(itemsArray))
+      location.reload()
     })
   })
 }
@@ -55,9 +100,11 @@ function activateDeleteListeners() {
   deleteBtn.forEach((db, i) => {
     db.addEventListener('click', () => {
       //Llamar la función que elimina la tarea
+      borrarTarea(i);
     })
   })
 }
+
 
 // Codigo DOM #4
 
@@ -84,7 +131,15 @@ function activateEditListeners() {
     })
   })
 
-  prioritySelects.addEventListener('change', (event) => {
+  document.querySelectorAll("#priority").forEach((select,i) => {
+    select.addEventListener("change", event => {
+      itemsArray[i].priority = event.target.value;
+      localStorage.setItem('items', JSON.stringify(itemsArray))
+      location.reload();
+    });
+  })  
+
+  /*prioritySelects.addEventListener('change', (event) => {
     const selectedIndex = event.target.selectedIndex
     itemsArray[i].priority = event.target.options[selectedIndex].text
     localStorage.setItem('items', JSON.stringify(itemsArray))
@@ -94,7 +149,7 @@ function activateEditListeners() {
     const selectedIndex = event.target.selectedIndex
     itemsArray[i].category = event.target.options[selectedIndex].text
     localStorage.setItem('items', JSON.stringify(itemsArray))
-  })
+  })*/
 }
 
 // Codigo DOM #5
@@ -106,9 +161,13 @@ function activateSaveListeners() {
   saveBtn.forEach((sB, i) => {
     sB.addEventListener('click', () => {
       // Llamar la función que guarda la actualización la tarea
+      console.log(inputs[i].value, i);
+      actualizarTarea(inputs[i].value, i);
     })
   })
 }
+
+
 
 // Codigo DOM #6
 // Esta es la lógica para el botón "cancelar" cuando presionas editar una tarea, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
@@ -122,10 +181,78 @@ function activateCancelListeners() {
       updateController[i].style.display = 'none'
       inputs[i].disabled = true
       inputs[i].style.border = 'none'
+      location.reload()
     })
   })
 }
 
+function displayItems() {
+  let items = ''
+  for (let i = 0; i < itemsArray.length; i++) {
+    items += `    <div class="item">
+                    <div class="input-controller">
+                      <input class="toggle" type="checkbox" id="check_${i}" ${
+      itemsArray[i].checked ? 'checked' : ''
+    } />
+                      <textarea disabled>${itemsArray[i].thing}</textarea>
+                      <div class="edit-controller">
+                        <div>
+                          Prioridad
+                          <select id="priority">
+                            <option ${
+                              itemsArray[i].priority === 'Alta'
+                                ? 'selected'
+                                : ''
+                            }>Alta</option>
+                            <option ${
+                              itemsArray[i].priority === 'Media'
+                                ? 'selected'
+                                : ''
+                            }>Media</option> 
+                            <option ${
+                              itemsArray[i].priority === 'Baja'
+                                ? 'selected'
+                                : ''
+                            }>Baja</option> 
+                          </select>
+                        </div>
+                        <div>
+                          Categorías
+                          <select id="category">
+                              <option ${
+                                itemsArray[i].category === 'Casa'
+                                  ? 'selected'
+                                  : ''
+                              }>Casa</option> 
+                              <option ${
+                                itemsArray[i].category === 'Trabajo'
+                                  ? 'selected'
+                                  : ''
+                              }>Trabajo</option> 
+                              <option ${
+                                itemsArray[i].category === 'Emprendimiento'
+                                  ? 'selected'
+                                  : ''
+                              }>Emprendimiento</option> 
+                            </select>
+                        </div>
+                        <i class="fa-solid fa-pen-to-square editBtn"></i>
+                        <i class="fa-solid fa-x deleteBtn"></i>
+                      </div>
+                    </div>
+                    <div class="update-controller">
+                    <button class="saveBtn">Save</button>
+                     <button class="cancelBtn">Cancel</button>
+                    </div>
+                  </div>`
+  }
+  document.querySelector('.todo-list').innerHTML = items
+  activateCheckboxListeners()
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
+}
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
 
 //El sistema debe permitir ELIMINAR una tarea.
@@ -146,4 +273,6 @@ function activateCancelListeners() {
 
 //Recordar llamar las funciones displayItems() y displayFooter() para mostrar
 //las tareas en pantalla
+
+displayItems()
 displayFooter()
